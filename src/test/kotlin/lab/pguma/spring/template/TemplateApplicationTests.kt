5a86c1158503618eda5524060d6f9398e8046221
pguma <p.guma.service+git@gmail.com>
2024-02-14 23:33:00 +0900

    init from spring initializer

---
+++ src/test/kotlin/lab/pguma/spring/template/TemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package lab.pguma.spring.template
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
